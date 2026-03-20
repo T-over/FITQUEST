2175da903dd238575361d4493ce98e3551853048
T-over <109075209+T-over@users.noreply.github.com>
2026-03-20 14:55:08 +0100

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'fitquest-v2';
+const ASSETS = [
+  '/FITQUEST/',
+  '/FITQUEST/index.html',
+  '/FITQUEST/manifest.json',
+  '/FITQUEST/icon-192.png',
+  '/FITQUEST/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return response;
+      }).catch(() => caches.match('/FITQUEST/index.html'));
+    })
+  );
+});
